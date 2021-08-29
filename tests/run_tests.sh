ea5dae7161330550f7a95b394a66a07f5460aab4
MisanthropicBit <albo.developer@gmail.com>
2021-08-29 12:33:13 +0200

    Add test runner script

---
+++ tests/run_tests.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ -f "$VADER_OUTPUT_FILE" ]; then
+    # Remove the old vader.log file if it exists
+    rm $VADER_OUTPUT_FILE
+fi
+
+TESTVIM_OPTS=
+
+# Neovim is always "nocompatible"
+if [ "$TESTVIM" = "vim" ]; then
+    TESTVIM_OPTS=-N
+fi
+
+$TESTCMD $TESTVIM $TESTVIM_OPTS -u ./tests/test_vimrc -c 'Vader! ./tests/*.vader'
+result=$?
+
+if [ -f "$VADER_OUTPUT_FILE" ]; then
+    cat $VADER_OUTPUT_FILE
+fi
+
+exit $result
